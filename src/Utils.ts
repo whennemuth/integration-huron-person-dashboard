4485802408063ac2fd8ca2962aee54fae229aa90
Hennemuth <wrh@bu.edu>
2026-01-22 18:28:35 -0500

    BUGFIX: Fixing synth npm script so that it properly pipes yaml to the target output file

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,3 +1,5 @@
+import { IContext } from "../context/IContext";
+
 const isBlank = (s:string|null|undefined):boolean => {
   return s === undefined || s === null || `${s}`.trim() == '';
 }
@@ -10,4 +12,18 @@ const someBlankSomeNot = (...a:any) => anyBlank(...a) && anyNotBlank(...a);
 
 export const ParameterTester = {
   isBlank, isNotBlank, anyBlank, anyNotBlank, allBlank, noneBlank, someBlankSomeNot
+}
+
+/**
+ * @returns The name of the stack
+ */
+export const getStackName = (context:IContext):string => {
+  const { STACK_ID, TAGS: { Landscape } } = context;
+  return `${STACK_ID}-${Landscape}`;
+}
+
+export const echoStackName = () => {
+  const contextModule = require('../context/context.json') as IContext;
+  const stackName = getStackName(contextModule);
+  console.log(stackName);
 }
